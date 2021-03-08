c669da9f0eef1241d5fdeee09bfb9752db0be6ad
k-alexey19 <dev.alexey19@gmail.com>
2021-03-08 17:21:42 +0400

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,13 +4,13 @@ const activeSlide = 'slider--active'
 const onClickSlide = e =>
 {
   const target = e.target
-  console.log(target)
-  const slideId = target.getAttribute('data-id')
+  const dataId = target.getAttribute('data-id')
+
   document.querySelector(`.slider__btn.${activeSlide}`).classList.remove(activeSlide)
   target.classList.add(activeSlide)
-
+  // СДЕЛАТЬ СРАВНЕНИЕ ПО АЙДИ ИЛИ ЕЩЕ КАК-ТО ЧТОБЫ УБИРАЛСЯ СЛАЙД В ДАННОМ СТЕПЕ
   document.querySelector(`.slider__slide.${activeSlide}`).classList.remove(activeSlide)
-  document.querySelector(`.slider__slide#${slideId}`).classList.add(activeSlide)
+  document.querySelector(`.slider__slide#${dataId}`).classList.add(activeSlide)
 }
 
 btnsSlider.map(btn => btn.onclick = e => onClickSlide(e))
