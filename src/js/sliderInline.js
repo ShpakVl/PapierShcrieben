dbb3e9f81f4f62235ab8874ba5a039695467d071
k-alexey19 <dev.alexey19@gmail.com>
2021-03-08 12:10:24 +0400

    slider-inline

--- src/js/sliderInline.js
+++ src/js/sliderInline.js
@@ -1,14 +1,39 @@
 const btns = Array.from(document.querySelectorAll('.slider-inline__number'))
 const divisions = document.querySelectorAll('.slider-inline__division')
-const slides = document.querySelectorAll('.slide')
+const slides = Array.from(document.querySelectorAll('.slider-inline__slide'))
 
+const active = 'slider-inline--active'
+
+const setActive = (target) =>
+{
+  target.classList.remove(active)
+  target.classList.add(active)
+  const prev = target.previousElementSibling
+
+  prev ? setActive(prev) : false
+}
 
 const onClick = e =>
 {
   const step = e.target.parentElement
-  const parent = step.parentElement
+  const slideId = step.getAttribute('slideId')
+  console.log(slideId)
+  btns.map(btn => 
+    {
+      const parent = btn.parentElement
+      
+      if (parent.classList.contains(active))
+      {
+        parent.classList.remove(active)
+        parent.previousElementSibling ? parent.previousElementSibling.classList.remove(active) : false
+      }
+    })
 
-  
+  setActive(step)
+
+  slides.map(slide => slide.classList.remove(active))
+
+  document.querySelector(`.slider-inline__slide#${slideId}`).classList.add(active)
 }
 
 btns.map(btn => btn.onclick = e => onClick(e))
