8fb91b1c61394bb4c4a5c2f815ac2c92a7360724
k-alexey19 <dev.alexey19@gmail.com>
2021-03-08 13:57:14 +0400

    slider js

---
+++ src/js/slider.js
@@ -0,0 +1,18 @@
+const bnts = Array.from(document.querySelectorAll('.slider__btn'))
+const slides = Array.from(document.querySelectorAll('.slider__slide'))
+
+const active = 'slider--active'
+
+const onClick = e =>
+{
+  const target = e.target
+  const slideId = target.getAttribute('data-id')
+
+  document.querySelector(`.slider__btn.${active}`).classList.remove(active)
+  target.classList.add(active)
+
+  document.querySelector(`.slider__slide.${active}`).classList.remove(active)
+  document.querySelector(`.slider__slide#${slideId}`).classList.add(active)
+}
+
+btns.map(btn => btn.onclick = e => onClick(e))
