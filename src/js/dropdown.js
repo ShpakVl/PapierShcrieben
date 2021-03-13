4cfe31383356c5af396139c324c44934e64876df
k-alexey19 <dev.alexey19@gmail.com>
2021-03-13 17:03:26 +0400

    refactor open\close arrow in dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,13 +1,14 @@
 const dropdowns = Array.from(document.querySelectorAll('.dropdown'))
 const options = Array.from(document.querySelectorAll('.dropdown__item'))
 const classActive = 'dropdown--open'
+const classArrow = 'arrow--open'
 
 const toggleMenu = target => {
     const active = document.querySelector(`.${classActive}`)
 
     active && active !== target ? active.classList.toggle(classActive) : false
 
-    !target.classList.contains('dropdown__item') ? target.classList.toggle(classActive) : false
+    !target.classList.contains('dropdown__item') ? [classActive, classArrow].map(cl => target.classList.toggle(cl)) : false
 }
 
 const selectOption = target => {
@@ -21,7 +22,7 @@ const selectOption = target => {
 }
 const onBlur = () => {
     const active = document.querySelector(`.${classActive}`)
-    active.classList.toggle(classActive);
+    active ? active.classList.toggle(classActive) : false
 }
 dropdowns.map(dropdown => {
     dropdown.onclick = e => toggleMenu(e.target)
